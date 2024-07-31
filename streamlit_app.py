90fe916918362dc87502d6d4d663573b54718038
aae-be <andreaalberto.egger@bearingpoint.com>
2024-07-31 11:51:12 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,13 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered {name_on_order}!', icon="✅")
-        
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
+
 # st.write("You selected:", options)
 # # Get the current credentials
 # session = SMOOTHIES.PUBLICtive_session()
